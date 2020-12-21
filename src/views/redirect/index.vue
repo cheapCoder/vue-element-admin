016194d9ed7c9b99ddd52fd6db44e8ceb9dc2685
liheng <1061555401@qq.com>
2020-12-21 17:58:46 +0800

    路由和侧边栏

--- src/views/redirect/index.vue
+++ src/views/redirect/index.vue
@@ -1,12 +1,13 @@
 <script>
 export default {
   created() {
+    //在重新定向回原来相同的页面，实现相同路由跳转能够刷新组件的效果
     const { params, query } = this.$route
     const { path } = params
     this.$router.replace({ path: '/' + path, query })
   },
   render: function(h) {
-    return h() // avoid warning message
+    return h() // 避免警告信息，组件必须有`template`或者`render`
   }
 }
 </script>
